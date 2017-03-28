f74a97ca9c2bb518f80f8975e8281f0b10896e2f
Anthony Phillips <aphillips196@gmail.com>
2017-03-28 14:43:56 -0400

    Modifying FastestRunner.java

--- project_5/FastestRunner.java
+++ project_5/FastestRunner.java
@@ -1,6 +1,7 @@
 // Uses StringUtils.rightPad from the Apache Commons Lang library v3.5
 // https://commons.apache.org/proper/commons-lang/download_lang.cgi
 import org.apache.commons.lang3.StringUtils;
+import java.util.Arrays;
 
 // Anthony Phillips
 // Project 5
@@ -11,99 +12,100 @@ import org.apache.commons.lang3.StringUtils;
 public class FastestRunner{
 
     public static void main(String[] args){
-        // I'd really prefer to make a "Runner" class and populate a single
-        // array of such objects, but the assignment specifies to do this:
-        String[] names = {
-            "Elena",    "Thomas",   "Hamilton", "Suzie",
-            "Phil",     "Matt",     "Alex",     "Emma",
-            "John",     "James",    "Jane",     "Emily",
-            "Daniel",   "Neda",     "Aaron",    "Kate"
-        };
+            Runner[] runners = {
+                new Runner("Elena", 341),
+                new Runner("Thomas", 273),
+                new Runner("Hamilton", 278),
+                new Runner("Suzie", 329),
+                new Runner("Phil", 445),
+                new Runner("Matt", 402),
+                new Runner("Alex", 388),
+                new Runner("Emma", 275),
+                new Runner("John", 243),
+                new Runner("James", 334),
+                new Runner("Jane", 412),
+                new Runner("Emily", 393),
+                new Runner("Daniel", 299),
+                new Runner("Neda", 343),
+                new Runner("Aaron", 317),
+                new Runner("Kate", 265),
+            };
 
-        Integer[] times = {
-            341,    273,    278,    329,
-            445,    402,    388,    275,
-            243,    334,    412,    393,
-            299,    343,    317,    265
-        };
+            System.out.println(Runner.asciiTable(runners));
 
-        System.out.println(runnerAsciiTable(names, times));
+            Arrays.sort(runners);
 
-        // If I could have, I would have done an Arrays.sort() on the Runner[]
-        // and then just called fastest with runners[0] and second-fastest with runners[1]
-        System.out.println("The fastest runner is: " + names[fastestIndex(times)]);
-        System.out.println("The second-fastest runner is: " + names[secondFastestIndex(times)]);
+            System.out.println(runners[0].toString());
+            System.out.println(runners[1].toString());
     }
 
-    // Prints an ascii-style table containing the
-    // names of runners and their corresponding times
-    private static String runnerAsciiTable(String[] names, Integer[] times){
-        int longestNameLength = 0;
+    // Private Runner class
+    // Implements Comparable<T> and
+    private static class Runner
+        implements Comparable<Runner>{
 
-        for (String name : names){
-            int nameLength = name.length();
+        // Name (read-only)
+        private final String name;
 
-            if (nameLength > longestNameLength)
-                longestNameLength = nameLength;
+        public String getName(){
+            return this.name;
         }
 
-        int longestTimeLength = 0;
+        // Time in minutes (read-only)
+        private final Integer time;
 
-        for (Integer time : times){
-            int timeLength = time.toString().length();
-
-            if (timeLength > longestTimeLength)
-                longestTimeLength = timeLength;
+        public Integer getTime(){
+            return this.time;
         }
 
-        // +---+---+
-        String outputTable = "+" + StringUtils.rightPad("", longestNameLength, '-') + "+"
-            + StringUtils.rightPad("", longestTimeLength, '-') + "+\n";
+        // Constructor
+        public Runner(String name, Integer time){
+            this.name = name;
+            this.time = time;
+        }
 
-        for (int i = 0; i < names.length || i < times.length; i++){
-            // |Name|Time|
-            outputTable += "|" + StringUtils.rightPad(names[i], longestNameLength) + "|"
-                + StringUtils.rightPad(times[i].toString(), longestTimeLength) + "|\n";
+        // Prints an ascii-style table containing the
+        // names of runners and their corresponding times
+        public static String asciiTable(Runner[] runners){
+            int longestNameLength = 0;
+            int longestTimeLength = 0;
+
+            for (Runner runner : runners){
+                int nameLength = runner.getName().length();
+                int timeLength = runner.getTime().toString().length();
+
+                if (nameLength > longestNameLength)
+                    longestNameLength = nameLength;
+
+                if (timeLength > longestTimeLength)
+                    longestTimeLength = timeLength;
+            }
+
             // +---+---+
-            outputTable += "+" + StringUtils.rightPad("", longestNameLength, '-') + "+"
+            String outputTable = "+" + StringUtils.rightPad("", longestNameLength, '-') + "+"
                 + StringUtils.rightPad("", longestTimeLength, '-') + "+\n";
-        }
 
-        return outputTable;
-    }
-
-    // Returns index of the fastest runner
-    private static int fastestIndex(Integer[] times){
-        int fastestIndex = 0;
-        int fastestTime = Integer.MAX_VALUE;
-
-        for (int i = 0; i < times.length; i++){
-            int time = times[i];
-
-            if (time < fastestTime){
-                fastestTime = time;
-                fastestIndex = i;
+            for (Runner runner : runners){
+                // |Name|Time|
+                outputTable += "|" + StringUtils.rightPad(runner.getName(), longestNameLength) + "|"
+                    + StringUtils.rightPad(runner.getTime().toString(), longestTimeLength) + "|\n";
+                // +---+---+
+                outputTable += "+" + StringUtils.rightPad("", longestNameLength, '-') + "+"
+                    + StringUtils.rightPad("", longestTimeLength, '-') + "+\n";
             }
+
+            return outputTable;
         }
 
-        return fastestIndex;
-    }
-
-    // Returns index of the second-fastest runner
-    private static int secondFastestIndex(Integer[] times){
-        int fastestTime = times[fastestIndex(times)];
-        int secondFastestIndex = 0;
-        int secondFastestTime = Integer.MAX_VALUE;
-
-        for (int i = 0; i < times.length; i++){
-            int time = times[i];
-
-            if (time < secondFastestTime && time > fastestTime){
-                secondFastestTime = time;
-                secondFastestIndex = i;
-            }
+        // Overridden toString()
+        public String toString(){
+            return String.format("%s, %dh %dm", this.getName(),
+                this.getTime() / 60, this.getTime() % 60);
         }
 
-        return secondFastestIndex;
+        // Implementation of Comparable<T>
+        public int compareTo(Runner o){
+            return this.getTime().compareTo(o.getTime());
+        }
     }
 }
